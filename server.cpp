e06e63e0cb0ec114eb6a541efaa6363d7fa12566
Azeruel <kevin.w.okada@gmail.com>
2018-11-30 08:46:54 -0700

    Add files via upload

--- server.cpp
+++ server.cpp
@@ -2,54 +2,48 @@
 #include "SocketException.h"
 #include "ClientSocket.h"
 #include <string>
-#include <thread>
+#include <pthread.h>
 
 
 
-void serverThreads(ServerSocket sSock){
+void* serverThreads(void *sSock){
+
+    ServerSocket tempSock = *(ServerSocket *) sSock;
+
     try{
         while (true){
             std::string data;
-            sSock>> data;
-            sSock<< data;
+            tempSock >> data;
+            tempSock << data;
         }
     }
     catch(SocketException&){
     }
-
+ pthread_exit(0);
 }
 
 
-int main()
+int main(int arg)
 
 {
+int threadNumb = 5;
 
 
-    try{
         ServerSocket server(30000);
 
 
-
-        while(true){
             ServerSocket new_sock;
             server.accept(new_sock);
 
-            std::thread th1(serverThreads,(new_sock), 1);
-            std::thread th2(serverThreads,(new_sock), 2);
-            std::thread th3(serverThreads,(new_sock), 3);
-            std::thread th4(serverThreads,(new_sock), 4);
-            std::thread th5(serverThreads,(new_sock), 5);
+        pthread_t td[threadNumb];
 
-            th1.join();
-            th2.join();
-            th3.join();
-            th4.join();
-            th5.join();
+        for (int i = 0; i < threadNumb; i++) {
+
+            pthread_create(&td[i], NULL, serverThreads, (void *) &new_sock);
+            pthread_join(td[i], NULL);
         }
-    }
-    catch (SocketException& e){
-        std::cout <<"exception was caught:" << e.description() << "\nExiting. \n";
-    }
+
+
     return 0;
 
 }
